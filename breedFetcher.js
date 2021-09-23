de9024ee13ffce63a6e5169681c07b584ec7f046
Sean Jin <seanhoyeonjin@gmail.com>
2021-09-23 21:27:25 +0000

    breed fetcher challenge

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,35 @@
 const request = require("request");
 
-const args = process.argv.slice(2);
+const fetchBreedDescription = function (breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    // if request is failed, print the details of the error
+    if (error) {
+      callback(error, null);
+    } else {
+      const data = JSON.parse(body);
+      // if breed is not found, output a message. If there is no breed, then data will be empty array
+      if (data.length === 0) {
+        callback(null, "Breed is not found");
+      } else {
+        callback(null, data[0].description);
+      }
+    }
+  });
+};
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
-  // if request is failed, print the details of the error
-  if (error) {
-    console.log(error);
-    return;
-  }
-  const data = JSON.parse(body);
-  // if breed is not found, output a message. If there is no breed, then data will be empty array
-  if (data.length === 0) {
-    console.log("Breed is not found");
-    return;
-  }
-  console.log(data[0].description);
-});
+// const fetchBreedDescription = function(breedName) {
+//   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+//     // if request is failed, print the details of the error
+//     if (error) {
+//       return error;
+//     }
+//     const data = JSON.parse(body);
+//     // if breed is not found, output a message. If there is no breed, then data will be empty array
+//     if (data.length === 0) {
+//       return "Breed is not found";
+//     }
+//     return data[0].description;
+//   });
+// };
+
+module.exports = { fetchBreedDescription };
